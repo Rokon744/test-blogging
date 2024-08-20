393d9df341473c85fb5154308bc9f45c8e31548c
Rokon744 <roknujjamanripon@gmail.com>
2024-08-21 00:06:32 +0600

    css add

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,19 +1,8 @@
-module.exports = {
-  plugins: [
-    "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: [
-              "./pages/**/*.{js,ts,jsx,tsx}",
-              "./components/**/*.{js,ts,jsx,tsx}",
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : []),
-  ],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
 };
+
+export default config;
